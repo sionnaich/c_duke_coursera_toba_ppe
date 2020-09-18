fc45243976900fa975f6d97553ad6036863f345e
Fábio H <30963669+sionnaich@users.noreply.github.com>
2020-09-18 02:18:35 -0300

    did assignment 20

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void rotate (char matrix[10][10])
+{
+  char novamatriz[10][10] = {0};
+  int n = 10;
+  //construir a matriz temp, ja que vai dar muito trampo fazer in-place
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; i < 10; j++)
+    {
+      novamatriz[i][j] = matrix[i][j];
+    }
+  }
+  //agora faremos a rotacao
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      matrix[i][j] = novamatriz[j][n-1-i];
+    }
+  }
+}
