44feeb5385056e81998d6f71830a1dd831324761
Fábio H <30963669+sionnaich@users.noreply.github.com>
2020-09-18 03:06:26 -0300

    modificado rotate.c

--- 20_rot_matrix/rotate.c
+++ 20_rot_matrix/rotate.c
@@ -9,7 +9,7 @@ void rotate (char matrix[10][10])
   //construir a matriz temp, ja que vai dar muito trampo fazer in-place
   for (int i = 0; i < 10; i++)
   {
-    for (int j = 0; i < 10; j++)
+    for (int j = 0; j < 10; j++)
     {
       novamatriz[i][j] = matrix[i][j];
     }
