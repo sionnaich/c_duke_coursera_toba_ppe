4e39e691451bbb10090f12ce01efcb8391b38702
Fábio <sionnaich@github.com>
2020-09-20 01:51:10 -0300

    aaah

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -4,7 +4,7 @@
 
 unsigned power(unsigned x, unsigned y);
 
-void run_check(int x, int y, int expected_ans)
+void run_check(unsigned x, unsigned y, unsigned expected_ans)
 {
     if (power(x, y) != expected_ans)
     {
@@ -14,33 +14,9 @@ void run_check(int x, int y, int expected_ans)
 
 int main()
 {
-    double esperado = 0;
-    int c = 0;
-    int valores_x[] = { -3, -2, -1, 0, 1, 2, 3, 5, 10 };
-    int valores_y[] = { -1, 0, 1, 2, 3, 5, 9 };
-    for (int i = 0; i < 9; i++)
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            if (valores_x[i] == 0 && valores_y[j] < 0)
-            {
-                ;
-            }
-            else if (valores_x[i] == 0 && valores_y[j] == 0)
-            {
-                esperado = 1;
-                c = (int)esperado;
-                run_check(valores_x[i], valores_y[j], c);
-                //printf("%d^%d = %d\n", valores_x[i], valores_y[j], c);
-            }
-            else
-            {
-                esperado = pow(valores_x[i], valores_y[j]);
-                c = (int)esperado;
-                run_check(valores_x[i], valores_y[j], c);
-                //printf("%d^%d = %d\n", valores_x[i], valores_y[j], c);
-            }
-        }
-    }
+    run_check(0, 0, 1);
+    run_check(1, 1, 1);
+    run_check(0, 10, 0);
+    run_check(1, -1, 1);
     return EXIT_SUCCESS;
 }
